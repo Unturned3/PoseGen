411325fa3f1a8374471d4ad65f12666c398e5bb6
Yusen Hu <richardyusen@gmail.com>
2024-05-25 15:47:33 +0100

    Read simulation params from external config file

--- gen_traj.py
+++ gen_traj.py
@@ -1,29 +1,15 @@
 
+import sys, time
+from os.path import join as pjoin
+from timer import Timer
 import cv2
 import numpy as np
 from numpy.linalg import norm as vec_norm
 import utils
-from timer import Timer
 import scipy.stats as ss
-import time
+import yaml
 
-show_histogram = False
-
-if show_histogram:
-    import matplotlib.pyplot as plt
-
-# pyequilib uses a right-handed coordinate system: X forward, Y left, Z up.
-# However, this has the unintuitive effect that negative tilt (pitch) angles
-# will make the camera look up. So, we set z_down=True to fix this behavior.
-
-n_frames = 900
-dt = 1 / 30
-kp = 2
-drag = 1
-angular_err_tolerance = 5
-
-max_fov = 75
-min_fov = 15
+cfg = None
 
 def wrap_angles(a):
     """Wrap angles to be in range [-180, 180]"""
@@ -40,10 +26,13 @@ def angular_dist(a, b):
     return d
 
 def rand_pose(prev_pose=None, target_fov=None):
+
+    global cfg
+
     if prev_pose is None:
         pan = np.random.uniform(-180, 180)
-        tilt = np.random.uniform(-5, 0)
-        roll = np.random.uniform(-5, 5)
+        tilt = np.random.uniform(cfg['min_init_tilt'], cfg['max_init_tilt'])
+        roll = np.random.uniform(cfg['min_init_roll'], cfg['max_init_roll'])
     else:
         assert target_fov is not None
         pp, pt, pr = prev_pose
@@ -59,10 +48,14 @@ def rand_pose(prev_pose=None, target_fov=None):
 
         roll = utils.multimodal(ss.norm, [pr-2, pr+2], [1, 1])
         roll = np.clip(roll, -5, 5)
+
     return wrap_angles([pan, tilt, roll])
 
 def simulate():
-    cur_fov = 75
+
+    global cfg
+
+    cur_fov = cfg['init_fov']
     cur_pose = rand_pose()
     angular_vel = np.array([0, 0, 0], np.float64)
     angular_accel = np.array([0, 0, 0], np.float64)
@@ -74,7 +67,7 @@ def simulate():
 
     while not end_simulation:
 
-        if np.random.uniform(0, 1) < 0.75:
+        if np.random.uniform(0, 1) < cfg['zoom_prob']:
             target_fov = utils.multimodal(
                 ss.norm, [35, 55, 75], [0] * 3)
         else:
@@ -87,26 +80,26 @@ def simulate():
             cur_fov += (1 - 2 * (cur_fov > target_fov)) * min(1, abs(cur_fov - target_fov))
 
             # Higher zoom = slower rotation
-            max_angular_vel = np.random.normal(30 - 0.6 * (max_fov - cur_fov), 2)
+            max_angular_vel = np.random.normal(30 - 0.6 * (cfg['max_fov'] - cur_fov), 2)
 
             angular_err = angular_dist(cur_pose, target_pose)
 
-            if vec_norm(angular_err) < angular_err_tolerance:
+            if vec_norm(angular_err) < cfg['angular_err_tolerance']:
                 break
 
             angular_err *= 50 / vec_norm(angular_err)
 
-            noise_vector = np.random.normal([0,0,0], [50,50,50])
+            noise_vector = np.random.normal([0,0,0], cfg['noise_scale'])
 
-            angular_accel = kp * angular_err + noise_vector
-            damping = drag * angular_vel
+            angular_accel = cfg['kp'] * angular_err + noise_vector
+            damping = cfg['drag'] * angular_vel
 
-            angular_vel += (angular_accel - damping) * dt
+            angular_vel += (angular_accel - damping) * cfg['dt']
 
             if vec_norm(angular_vel) > max_angular_vel:
                 angular_vel *= max_angular_vel / vec_norm(angular_vel)
 
-            cur_pose = (cur_pose + angular_vel * dt) % 360
+            cur_pose = (cur_pose + angular_vel * cfg['dt']) % 360
             cur_pose[cur_pose > 180] -= 360
 
             fovs.append(cur_fov)
@@ -114,16 +107,8 @@ def simulate():
 
             frame_cnt += 1
 
-            if frame_cnt >= n_frames:
+            if frame_cnt >= cfg['n_frames']:
                 end_simulation = True
-
-    if show_histogram:
-        pan_angles = [e[0] for e in poses]
-        tilt_angles = [e[1] for e in poses]
-        plt.hist(pan_angles)
-        plt.hist(tilt_angles)
-        plt.hist(fovs)
-        plt.show()
 
     return poses, fovs
 
@@ -134,11 +119,20 @@ def save_trajectory(name, poses, fovs):
 
 
 def main():
+
+    global cfg
+
     np.set_printoptions(precision=3, suppress=True)
 
-    for i in range(25):
+    with open(sys.argv[1], 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    n_trajs = int(sys.argv[2])
+    out_dir = sys.argv[3]
+
+    for i in range(n_trajs):
         poses, fovs = simulate()
-        save_trajectory(f'/Users/richard/Desktop/Trajs/t{i:03d}.npy', poses, fovs)
+        save_trajectory(pjoin(out_dir, f'{i:03d}.npy'), poses, fovs)
 
 if __name__ == '__main__':
     main()
